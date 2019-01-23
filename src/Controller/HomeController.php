dd11bebce8e1cb7065d88df1e1973970ba1e7284
tmillot08 <thomas.millot08@gmail.com>
2019-01-23 13:01:00 +0100

    tytyr
    tretret

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,18 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\GameRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(GameRepository $repo)
     {
+        $gamelist = $repo->findBy();
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
